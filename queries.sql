3ce0bf63715d278bf6ebb8d38279a5fd2d3b26dd
Dave-Clark-02 <56134330+Dave-Clark-02@users.noreply.github.com>
2020-02-06 22:15:42 -0600

    through step 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,10 @@
-## Part 1: Test it with SQL
+--## Part 1: Test it with SQL
 
-## Part 2: Test it with SQL
+--## Part 2: Test it with SQL
+SELECT name from employer
+WHERE location = 'St. Louis, MO';
+--## Part 3: Test it with SQL
+DROP TABLE job;
 
-## Part 3: Test it with SQL
-
-## Part 4: Test it with SQL
+--## Part 4: Test it with SQL
+SELECT name, description FROM techjobs.skill WHERE id in(SELECT skills_id FROM job_skills WHERE skills_id IS NOT NULL);
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,10 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
+
+
+  @OneToMany
+  @JoinColumn
+   private List<Job> jobs= new ArrayList<>();
 
     @Valid
     public String location;
